ba05165d3417d13b2e651016c34ac6c356ffd096
bazanga <briank621@gmail.com>
2014-02-08 02:26:55 -0500

    this is new run

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, request, redirect
 import twilio.twiml
+import send_sms as send
 
 #The session object makes use of a secret key.
 app = Flask(__name__)
@@ -10,18 +11,27 @@ unpaired = 0
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    if(request.values.get('From') in session):   
+    incoming = request.values.get('From')
+    #if this is a first-time request
+    if(! incoming in session):   
         if unpaired == 0:
-	    unpaired = request.values.get('From')
-	    session[request.values.get('From')] = 0	
+	    unpaired = incoming
+	    session[incoming] = [request.values.get('Body')]	
         else:
-   	    session[request.value.get('From')] = unpaired
+   	    session[incoming] = [request.values.get('Body'),unpaired]
+	    session[unpaired].append(incoming)
+	    send_message(unpaired, 'You have been connected to a partner (session[incoming][1])')
+	    send_message(incoming, 'You have been connected to a partner (session[unpaired][1])')
 	    unpaired = 0
+    #already established partner (sending message)
     else:
-    
+    	send_message(session[incoming][1], sessionrequest.value.get('Body'))
     resp = twilio.twiml.Response()
     resp.message("Hello, Monkey")
     return str(resp)
+
+def send_message():
+    
 
 if __name__ == "__main__":
     app.secret_key = "A0zro0!!asdl?,whatthe?'
--- send_sms.py
+++ send_sms.py
@@ -1,2 +1,10 @@
 from twilio.rest import TwilioRestClient
 
+account_sid = "AC194d442b38cb4ff37c8516106d092d90"
+auth_token = "105ec4bc266c188259ea6669338ef0de"
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.sms.messages.create(body="Brian please?! I love you <3", 
+	to="16465964681",
+	from_="18458759296")
+print message.sid
