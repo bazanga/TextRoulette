472d9c39c7d4b50c620c34ccfde07ea401d8fdde
bazanga <briank621@gmail.com>
2014-02-08 04:29:38 -0500

    Fixed logical problems

--- send_sms.py
+++ send_sms.py
@@ -5,11 +5,5 @@ auth_token = "105ec4bc266c188259ea6669338ef0de"
 client = TwilioRestClient(account_sid, auth_token)
 
 def send_message(number, message):
-    if (message.lower() == 'end')
-	return -1
-    if (message.lower() = 'stop')
-	return -2
     message = client.sms.messages.create(body=message, to=number, from_="18458759296")
     print message.sid
-    return 1
-
