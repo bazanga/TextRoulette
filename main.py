23aa9a40a66d4edd65ac96ceb471617dc42326bc
Edward Li <el2724@columbia.edu>
2014-02-08 14:17:41 +0000

    IT WORKS

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
 
-from flask import Flask, session, request, redirect
+from flask import Flask, request, redirect
 import twilio.twiml
 import send_sms as send
-
+session = {}
 #The session object makes use of a secret key.
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -18,43 +18,50 @@ unpaired = []
 def hello_monkey():
     incoming = request.values.get('From')
     #if this is not a first time request
-    if(incoming in session):   
+    if(incoming in session):
 	outgoing = session[incoming][1]
 	body = request.values.get('Body')
-        #He wants to move to next convo
-        if(body == 'END'):
-	    del session[incoming]
-	    if(incoming in unpaired):
-		return
-	    if(len(unpaired) == 0):
-		remove_partner(outgoing)
-		unpaired.append(outgoing)
+       #He wants to move to next convo
+        if(body == 'PEACE'): 
+            send.send_message(incoming, 'You quit.')
+            if(incoming in unpaired):
+                unpaired.remove(incoming)
+                return
+            elif (len(unpaired) == 0):
+                send.send_message(outgoing, 'Your partner quit.')
+                unpaired.append(outgoing)
 	    else:
-		set_partner(outgoing, unpaired.pop(0))
+                send.send_message(outgoing, 'Your partner quit.')
+                set_partner(outgoing, unpaired.pop(0))
+            del session[incoming]
         elif(body == 'NEXT'):
 	    if(incoming in unpaired):
 		return
 	    if(len(unpaired) == 0):
-		remove_partner(incoming)
-		remove_partner(outgoing)
 		unpaired.append(incoming)
 		unpaired.append(outgoing)
+                send.send_message(incoming, 'Waiting for next...')
+                send.send_message(outgoing, 'Your partner moved on. Waiting for next...')
 	    elif(len(unpaired) == 1):
-		remove_partner(incoming)
+                send.send_message(outgoing, 'Your partner moved on.')
 		set_partner(outgoing, unpaired.pop())
 		unpaired.append(incoming)
+                send.send_message(incoming, 'Waiting for next...')
 	    elif(len(unpaired) == 2):
+                send.send_message(incoming, 'You left.')
+                send.send_message(outgoing, 'Your partner left.')
 		set_partner(outgoing, unpaired.pop())
 		set_partner(incoming, unpaired.pop())
+        
 	elif not (incoming in unpaired):
 	    send.send_message(outgoing, body)
-    else: 
-	session[incoming] = [request.values.get('Body')]
+    else:
+	session[incoming] = [request.values.get('Body'), '']
+        send.send_message(incoming, 'Username: ' + request.values.get('Body'))
         if len(unpaired) == 0:
 	    unpaired.append(incoming)
         else:
-	    outgoing = unpaired.pop()
-   	    set_partner(incoming, outgoing)
+   	    set_partner(incoming, unpaired.pop(0))
     return ''
  
 def set_partner(a, b):
@@ -62,17 +69,14 @@ def set_partner(a, b):
     session[b][1] = a
     send_initial(a, b)
 
-def remove_partner(a):
-    session[a].pop()
-
 def send_partnerquit(outgoing):
     msg = 'Your partner has disconnected. Waiting for a new partner...'
     send.send_message(outgoing, msg)
 
 def send_initial(incoming, outgoing):
-    msg = 'You have been connected to a partner ('
-    send.send_message(incoming, msg + session[outgoing][0] + ')')
-    send.send_message(outgoing, msg + session[incoming][0] + ')')
+    msg = 'You have been connected to a partner: '
+    send.send_message(incoming, msg + session[outgoing][0])
+    send.send_message(outgoing, msg + session[incoming][0])
 
 if __name__ == "__main__": 
     app.run(debug=True)
--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,13 @@
 from twilio.rest import TwilioRestClient
 
-account_sid = "AC00e3ac5173e7bd97b486deeb151b9eb9"
-auth_token = "53df96318d6c0d49d9bd32e826a6"
+##from_="+13473216937"
+#account_sid = "AC00e3ac5173e7bd97b486deeb151b9eb9"
+#auth_token = "53df96318d6c0d493c49d9bd32e826a6"
+account_sid = "AC194d442b38cb4ff37c8516106d092d90"
+auth_token = "105ec4bc266c188259ea6669338ef0de"
 client = TwilioRestClient(account_sid, auth_token)
 
 def send_message(number, message):
-    message = client.sms.messages.create(body=message, to=number, from_="+13473216937")
-    print message.sid
+    message = client.messages.create(to=number,from_="+18458759296", body=message)
+
+
