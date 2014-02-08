66302436b0ffff2f157fde0aabb9d3d2fccb642b
bazanga <briank621@gmail.com>
2014-02-08 03:20:02 -0500

    Added multiple functionality for the unpaired list

--- run.py
+++ run.py
@@ -7,31 +7,45 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 #The unpaired phone number
-unpaired = 0
+unpaired = []
 
+#Function to make sure at most one person is unpaired
+def setPairs()
+    for x in range(0, len(unpaired)/2):
+	session[unpaired[x * 2]][1] = unpaired[x + 1]
+	session[unpaired[x * 2 + 1]][1] = unpaired[x]
+	send_initial(unpaired[x*2], unpaired[x*2 + 1])
+	unpaired.pop(x)
+	unpaired.pop(x + 1)
+       
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+    if(len(unpaired) > 1):
+	setPairs()
     incoming = request.values.get('From')
     #if this is a first-time request
     if(! incoming in session):   
-        if unpaired == 0:
-	    unpaired = incoming
+        if len(unpaired) == 0:
+	    unpaired.append(incoming)
 	    session[incoming] = [request.values.get('Body')]	
         else:
-   	    session[incoming] = [request.values.get('Body'),unpaired]
-	    session[unpaired].append(incoming)
-	    send_message(unpaired, 'You have been connected to a partner (session[incoming][1])')
-	    send_message(incoming, 'You have been connected to a partner (session[unpaired][1])')
-	    unpaired = 0
+	    outgoing = unpaired.pop()
+   	    session[incoming] = [request.values.get('Body'),outgoing]
+	    session[outgoing].append(incoming)
+	    send_initial(outgoing, incoming)
     #already established partner (sending message)
     else:
-    	send_message(session[incoming][1], sessionrequest.value.get('Body'))
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Monkey")
-    return str(resp)
+    	ans = send.send_message(session[incoming][1], sessionrequest.value.get('Body'))
+    #He wants to stop the convo
+    if(ans = -1):
+	session[incoming].pop()
+	unpaired.append(incoming)
+    if(ans = -2):
 
-def send_message():
-    
+def send_initial(incoming, outgoing):
+    msg = 'You have been connected to a partner ('
+    send.send_message(incoming, msg + session[outgoing][1] + ')')
+    send.send_message(outgoing, msg + session[incoming][1] + ')')
 
 if __name__ == "__main__":
     app.secret_key = "A0zro0!!asdl?,whatthe?'
--- send_sms.py
+++ send_sms.py
@@ -4,7 +4,12 @@ account_sid = "AC194d442b38cb4ff37c8516106d092d90"
 auth_token = "105ec4bc266c188259ea6669338ef0de"
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.sms.messages.create(body="Brian please?! I love you <3", 
-	to="16465964681",
-	from_="18458759296")
-print message.sid
+def send_message(number, message):
+    if (message.lower() == 'end')
+	return -1
+    if (message.lower() = 'stop')
+	return -2
+    message = client.sms.messages.create(body=message, to=number, from_="18458759296")
+    print message.sid
+    return 1
+
