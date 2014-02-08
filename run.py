05d3885aa085502239ef1bec4b7105f04db15789
Edward Li <el2724@columbia.edu>
2014-02-08 10:16:20 +0000

    added google app engine PYTHONPATH modification to run.py

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+
+
 from flask import Flask, session, request, redirect
 import twilio.twiml
 import send_sms as send
