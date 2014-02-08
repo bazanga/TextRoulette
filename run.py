766d0f9e8011dee9124a49a762a513c3e206df13
bazanga <briank621@gmail.com>
2014-02-08 01:42:34 -0500

    Added basic session stuff

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
-from flask import Flask, request, redirect
+from flask import Flask, session, request, redirect
 import twilio.twiml
 
+#The session object makes use of a secret key.
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+#The unpaired phone number
+unpaired = 0
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+    if(request.values.get('From') in session):   
+        if unpaired == 0:
+	    unpaired = request.values.get('From')
+	    session[request.values.get('From')] = 0	
+        else:
+   	    session[request.value.get('From')] = unpaired
+	    unpaired = 0
+    else:
+    
     resp = twilio.twiml.Response()
     resp.message("Hello, Monkey")
     return str(resp)
 
 if __name__ == "__main__":
+    app.secret_key = "A0zro0!!asdl?,whatthe?'
     app.run(debug=True)
